9aa3c9226f742230064190f49b1174d86d601437
unknown <jmnajm@ndu.edu.lb>
2023-03-05 15:56:16 +0200

    who has seen the wind

---
+++ CCC/CCC '11 J2 - Who Has Seen The Wind.py
@@ -0,0 +1,13 @@
+h = int(input(''))
+max_t = int(input(''))
+t = 1
+
+for _ in range(max_t):
+    a = (-6*t**4) + (h*t**3) + (2*t**2) + t
+    if a<=0:
+        print('The balloon first touches ground at hour:')
+        print(t)
+        break
+    if t == max_t:
+        print('The balloon does not touch ground in the given time.')
+    t+=1
